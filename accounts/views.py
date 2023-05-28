20e6e6edab94e044aaa096c3f4dcf341c57195b4
Doaa Sayed <doaas0213@gmail.com>
2023-05-28 19:51:03 +0300

    feat: registration, login & logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import RegistrationForm
+from django.urls import reverse_lazy
+from .models import User
 
-# Create your views here.
+
+class Registration(FormView):
+    template_name = "registration/register.html"
+    form_class = RegistrationForm
+    success_url = reverse_lazy("accounts:login")
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        User.objects.create_user(data["name"], data["email"], data["password1"])
+        return super().form_valid(form)
+
+
