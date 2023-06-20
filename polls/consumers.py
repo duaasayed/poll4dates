c6aff6eeadde512e011ab60d19aef5d92bd15d72
Doaa Sayed <doaas0213@gmail.com>
2023-06-20 14:17:00 +0300

    ui: fix jsdelivr

--- polls/consumers.py
+++ polls/consumers.py
@@ -30,3 +30,31 @@ class ChatConsumer(WebsocketConsumer):
     def chat_message(self, event):
         message = event['message']
         self.send(text_data=json.dumps({'message': message}))
+
+
+
+class DataConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['pk']
+        self.room_group_name = f"poll-{self.room_name}"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
+        self.accept()
+
+
+    def disconnect(self):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+
+    def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {'type': 'chat_message', 'message': message}
+        )
+        return super().receive(text_data, bytes_data)
