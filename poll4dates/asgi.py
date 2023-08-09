fd8c89bfbd627de35a297c326bfaf80b32e2f920
Doaa Sayed <doaas0213@gmail.com>
2023-08-09 12:15:23 +0300

    adding nginx server

--- poll4dates/asgi.py
+++ poll4dates/asgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import polls.routing
@@ -17,6 +19,6 @@ import polls.routing
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poll4dates.settings")
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(URLRouter(polls.routing.websocket_urlpatterns))
 })
