3c7e8b3dd6202cd4b28a3fc3b8704e1c1b0b68e6
Doaa Sayed <doaas0213@gmail.com>
2023-08-10 15:00:11 +0300

    fix: users cannot vote for closed polls

--- staticfiles/polls/js/votes_options.js
+++ staticfiles/polls/js/votes_options.js
@@ -10,71 +10,75 @@ votesbtn.addEventListener('click', () => {
     document.getElementById('votes-body').classList.remove('d-none')
 })
 
-calendarbtn.addEventListener('click', () => {
-    var calendarEl = document.getElementById('calendar');
-    if (tablebtn) {
-        document.getElementById('table-body').classList.add('d-none')
-    }
-    document.getElementById('votes-body').classList.add('d-none')
-    calendarEl.classList.remove('d-none')
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        selectable: true,
-        selectMirror: true,
 
-        editable: true,
-        eventDidMount: (cell) => {
-            console.log(cell.event)
-            var checkbox = document.createElement('input')
-            checkbox.type = 'checkbox'
-            checkbox.setAttribute('id', 'date-' + cell.event.id)
-            checkbox.setAttribute('style', 'transform: scale(1.5);accent-color:green')
-            cell.el.children[0].children[0].append(checkbox)
-            checkbox.value = cell.event.id
-            console.log(guest)
-            if (votes) {
-                votes.forEach(vote => {
-                    if ((parseInt(vote.fields.time_slot) == cell.event.id)) {
-                        checkbox.checked = true
-                    }
+if (calendarbtn) {
+    calendarbtn.addEventListener('click', () => {
+        var calendarEl = document.getElementById('calendar');
+        if (tablebtn) {
+            document.getElementById('table-body').classList.add('d-none')
+        }
+        document.getElementById('votes-body').classList.add('d-none')
+        calendarEl.classList.remove('d-none')
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            selectable: true,
+            selectMirror: true,
 
-                    var choice = document.getElementById(`choice-${cell.event.id}`)
-                    checkbox.checked = choice.checked
-                })
-                checkbox.addEventListener('change', () => {
-                    var timeslot = checkbox.value
+            editable: true,
+            eventDidMount: (cell) => {
+                console.log(cell.event)
+                var checkbox = document.createElement('input')
+                checkbox.type = 'checkbox'
+                checkbox.setAttribute('id', 'date-' + cell.event.id)
+                checkbox.setAttribute('style', 'transform: scale(1.5);accent-color:green')
+                cell.el.children[0].children[0].append(checkbox)
+                checkbox.value = cell.event.id
+                console.log(guest)
+                if (votes) {
+                    votes.forEach(vote => {
+                        if ((parseInt(vote.fields.time_slot) == cell.event.id)) {
+                            checkbox.checked = true
+                        }
 
-                    if (guest == null) {
-                        guestModal.style.display = 'block';
-                    }
+                        var choice = document.getElementById(`choice-${cell.event.id}`)
+                        checkbox.checked = choice.checked
+                    })
+                    checkbox.addEventListener('change', () => {
+                        var timeslot = checkbox.value
+
+                        if (guest == null) {
+                            guestModal.style.display = 'block';
+                        }
 
 
-                    voteSocket.send(JSON.stringify({
-                        'timeslot_id': parseInt(timeslot),
-                        'guest_id': parseInt(guest_id),
-                        'vote_method': 'calendar'
-                    }));
+                        voteSocket.send(JSON.stringify({
+                            'timeslot_id': parseInt(timeslot),
+                            'guest_id': parseInt(guest_id),
+                            'vote_method': 'calendar'
+                        }));
 
-                })
+                    })
+                }
             }
-        }
 
+        })
+        calendar.render()
+        var timeslots = JSON.parse(slots)
+        console.log(timeslots)
+        timeslots.forEach(timeslot => {
+            var event = {
+                'id': timeslot.pk,
+                'title': timeslot.fields.start.split(':', 2).join(':'),
+                'start': timeslot.fields.day
+            }
+            console.log(event)
+            calendar.addEvent(event)
+            console.log(calendar.getEvents())
+
+        });
     })
-    calendar.render()
-    var timeslots = JSON.parse(slots)
-    console.log(timeslots)
-    timeslots.forEach(timeslot => {
-        var event = {
-            'id': timeslot.pk,
-            'title': timeslot.fields.start.split(':', 2).join(':'),
-            'start': timeslot.fields.day
-        }
-        console.log(event)
-        calendar.addEvent(event)
-        console.log(calendar.getEvents())
+}
 
-    });
-})
 
 if (tablebtn) {
     tablebtn.addEventListener('click', () => {
